b38c3703cf25e2766eb5a7c8e1a130d6b1693983
swapnil <swapnilpolkar@gmail.com>
2022-04-16 01:07:52 +0530

    added: fibonacci series file

---
+++ FibonacciSeries.java
@@ -0,0 +1,15 @@
+package bridgelabz.com.day6;
+/*
+ * Authot: Swapnil
+ * program to print fibonacci series till nth term
+ */
+import utillity.util;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		util.fibonacciSeries();
+	}
+
+}
