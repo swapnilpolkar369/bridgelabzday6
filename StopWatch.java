08294eadea66b276335843729bee7b89335c9bb8
swapnil <swapnilpolkar@gmail.com>
2022-04-16 01:16:43 +0530

    added: stopwatch file

---
+++ StopWatch.java
@@ -0,0 +1,15 @@
+package bridgelabz.com.day6;
+/*
+ * Author: Swapnil
+ * Simulate stopwatch program
+ */
+import utillity.util;
+
+public class StopWatch {
+
+	public static void main(String[] args) {
+		
+		util.calculateTime();
+	}
+
+}
