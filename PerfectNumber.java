70540d08242219ccf992ad60b0b4cc1f4112cb5c
swapnil <swapnilpolkar@gmail.com>
2022-04-16 01:13:48 +0530

    added: perfectnumber file

---
+++ PerfectNumber.java
@@ -0,0 +1,15 @@
+package bridgelabz.com.day6;
+/*
+ * Author: Swapnil
+ * Program to check entered number is perfect number or not
+ */
+import utillity.util;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		
+		util.perfectNumber();
+	}
+
+}
