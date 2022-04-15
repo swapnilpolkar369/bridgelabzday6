aeaa3d6a137a77b7c1eb1eab443563bd264f6f69
swapnil <swapnilpolkar@gmail.com>
2022-04-16 01:16:01 +0530

    added: ReverseNumber file

---
+++ ReverseNumber.java
@@ -0,0 +1,15 @@
+package bridgelabz.com.day6;
+/*
+ * Author: Swapnil
+ * Program to reverse an entered number
+ */
+import utillity.util;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		util.reverseNumber();
+	}
+
+}
