35b1f89acbebcd7799245819c4ce80a4304b3fa0
swapnil <swapnilpolkar@gmail.com>
2022-04-16 01:13:02 +0530

    added: couponnumber file

---
+++ CouponNumber.java
@@ -0,0 +1,25 @@
+package bridgelabz.com.day6;
+
+import utillity.util;
+
+/*
+ * Author: Swapnil
+ * Program to generate random coupon number
+ */
+public class CouponNumber {
+
+	public class util {
+
+		public static void CouponNumber() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+		
+		util.CouponNumber();
+	}
+
+}
