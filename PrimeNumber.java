3b9b6d34ddf54331ee98c9a8eabcf5e479495515
swapnil <swapnilpolkar@gmail.com>
2022-04-16 01:14:36 +0530

    added: primenumber file

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+package bridgelabz.com.day6;
+/*
+ * Author: Swapnil
+ * Program to check entered number is prime number or not
+ */
+import utillity.util;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		util.checkPrimeNumber();
+	}
+
+}
